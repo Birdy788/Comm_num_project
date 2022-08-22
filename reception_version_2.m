da0d258792dc6298829f9ecf48142a53edb1bc70
Yassine Amrani <62746812+Birdy788@users.noreply.github.com>
2022-08-22 23:57:24 +0200

    Add files via upload

---
+++ reception_version_2.m
@@ -0,0 +1,224 @@
+%% Récepteur pi/4-DQPSK/QPSK avec pré-synchronisation temporelle et fréquentielle 
+clear all, clc, close all;
+
+%% Paramètres 
+%Te=2*1e-7; % temps d'échantillonnage des CNA et CAN
+Ts=2e-6; % temps symbole
+Fse = 6;
+Te = Ts/Fse;
+nb = 2; % nombre de bits/symbole
+fe=1/Te;
+Ds=1/Ts; % débit symbole
+Db=nb*Ds; % débit binaire
+M=2^nb;
+rows = 400; columns = 600;
+Nb=rows*columns*8*3;
+%Fse=floor(Ts/Te); % facteur de sous-echantillonnage
+f0=1.255e9; % porteuse pour le calcul du PL
+seuil=0.76;
+Ptx = 1; % Puissance d'émission (W)
+roll_off=0.8;
+A=127;
+Nb_paquets=48;
+Octet_verif = hexToBinaryVector('0xAA');
+len_filter = 49;
+n_conv = (((Nb+Nb_paquets*length(Octet_verif))/2/Nb_paquets)+3)*Fse+len_filter;
+
+
+%% Modes de réception
+
+thetaInit = pi/4; % Theta initial pour le mode différentiel
+ssd_Ts_1 = exp(1i*thetaInit); % premier symbol pilote pour correction de phase donc s_b(1:2) = [0 0]
+Nfft=4096;
+
+
+%% Préambule 
+preambule = [ones(1,Fse) zeros(1,Fse) ones(1,Fse) zeros(1,2*Fse) ones(1,Fse) zeros(1,Fse) ones(1,Fse) zeros(1,4*Fse-1)];
+preambule = repmat(preambule,1,2); % repmat() : répète le motif de la matrice préambule sur une fois en ligne et deux fois en colonne
+Ep = sum(preambule.^2); %énergie de la séquence de préambule
+
+%% Filtre adapté
+
+g=rcosdesign(roll_off,2*M, Fse, 'sqrt');
+ga = conj(fliplr(g)); % ga(t) = g(Ts-t) (le retard de Ts est implicite en numérique car on travaille avec des vecteurs causaux)
+
+%% Acquisition des données du récepteur 
+fileID = fopen('QPSK_Tx.raw','r');
+rl=fread(fileID,'int8=>double');
+fclose(fileID);
+
+rl_I=rl(1:2:end);
+rl_Q=rl(2:2:end);
+yl_Te=transpose(rl_I+1j*rl_Q);
+
+
+% % Canal
+k = 1.38e-23; % Cte Boltzmann
+T = 273.15 + 25; % Température bruit du récepteurans
+N0 = k*T;
+varb = N0/2;
+s=yl_Te;
+tau0=1000*rand;
+%tau0=1;
+tau0=floor(tau0); %retard dû à la propagation
+alpha0 = sqrt(1e-8);  % coefficient d'atténuation du canal
+h_l = alpha0*[zeros(1,tau0) 1]; %dirac décalé de tau0*Te
+Ps = 1/Te*mean(abs(s).^2);
+yl_Te = sqrt(Ptx/Ps)*yl_Te/127;
+y = conv(yl_Te,h_l); % signal en sortie du canal
+
+%% Recepteur
+Deltaf = 50e-6*f0*2*(rand-0.5); % erreur de fréquence OL précis à +-50ppm , 2*(rand-0.5)=nombre aléatoire dans l'intervalle [-1,1]
+%Deltaf = 0;
+n = sqrt(varb/2)*(randn(1,length(y))+1i*randn(1,length(y)));
+yl = y.*exp(1i*2*pi*[0:length(y)-1]*Te*Deltaf+1i*pi/8)+n ;
+yl_Te=yl;
+
+%% Recepteur
+yl_Te = yl_Te./norm(yl_Te); %% normalisation : utile ? => sûrement pour faire l'intercorrélation entre le préambule et le préambule reçu
+test=yl_Te;
+
+%% fonction d'interrcorr�lation
+rho=filter(fliplr(preambule),1,abs(yl_Te))./(sqrt(Ep)*sqrt(...
+    filter(ones(1,length(preambule)),1,abs(yl_Te).^2)));
+figure, plot(rho),
+title("fonction d'intercorr�lation")
+xlabel('Nombre d''�chantillons')
+ylabel ('?(u)')
+%% Pr�-synchronisation temporelle 
+rl_Ts_conc=0;
+for k=1:Nb_paquets %pb qui vient de la synchro tps
+    disp(k);
+    rho=filter(fliplr(preambule),1,abs(yl_Te))./(sqrt(Ep)*sqrt(...
+    filter(ones(1,length(preambule)),1,abs(yl_Te).^2)));
+    i=1;
+    while i < length(rho)
+        if rho(i)> seuil
+            indice = i;
+            break
+        end
+        i=i+1;    
+    end
+    temp =[];   
+    ind =[];
+
+    for j=indice:indice+n_conv
+        if rho(j)> seuil
+            temp = [temp,rho(j)];
+            ind = [ind,j];
+        end
+    end
+
+    M=max(temp);
+    indSynch = ind(find(temp==M));    
+    %disp(indSynch);
+    %disp(M);
+%     %Verification des indices de synchro
+%     rho=filter(fliplr(preambule),1,abs(yl_Te))./(sqrt(Ep)*sqrt(...
+%     filter(ones(1,length(preambule)),1,abs(yl_Te).^2)));
+% 
+%     figure, plot(rho)
+%     hold on,
+%     plot(indSynch,M,'r*')
+%     title("fonction d'intercorr�lation"),
+%     xlabel('temps')   
+    if (indSynch+((Nb+Nb_paquets*length(Octet_verif))/2/Nb_paquets+2)*Fse)>length(yl_Te)
+                yl_Te_synch = yl_Te(indSynch:end);  
+                yl_Te(indSynch-length(preambule)+1:end)=[];
+    else
+        yl_Te_synch = yl_Te(indSynch:indSynch+((Nb+Nb_paquets*length(Octet_verif))/2/Nb_paquets+2)*Fse+length(ga));
+        yl_Te(indSynch-length(preambule)+1:indSynch-length(preambule)+1+((Nb+Nb_paquets*length(Octet_verif))/2/Nb_paquets+2)*Fse+length(ga)-1)=[];
+
+    end
+    
+
+    %% Sélection du message utile
+          [Rl_Te, f] = pwelch(yl_Te_synch.^8, ones(1,Nfft), 0, Nfft, fe, 'centered');
+          %figure, pwelch(yl_Te_synch.^8, ones(1,Nfft), 0, Nfft, fe, 'centered');
+          [~, indice] = max(Rl_Te);
+          f0_8_esti = f(indice)/8;
+          disp(f0_8_esti);
+          yl_Te_synch=exp(-1j*(2*pi*f0_8_esti*(0:length(yl_Te_synch)-1)*Te)).*yl_Te_synch;		  
+          %% Filtre adapté
+          rl_Te = conv(yl_Te_synch,ga);  % convolution par le filtre adpaté         
+		  [ K1, K2 ] = piLoopConstants(calcTedKp('GTED', 0.8), -1, 1, 0, 1); %K0=-1
+          [ rl_Te_post ] = PLL_Loop('GTED', 1, rl_Te, K1, K2);
+           
+		  rl_Ts1=rl_Te_post(length(ga)+1:Fse:length(ga)+((Nb+Nb_paquets*length(Octet_verif))/2/Nb_paquets+1)*Fse+1); % sous echantillonnage au rythme Fse (correspond à un échantillonnage de r_l(t) au rythme Ts)
+          rl_Ts=transpose(rl_Ts1); % we transpose to have the same dimensions 
+          % Pré-synchronisation fréquentielle tant que 2pi*Nb*f_erreur<pi/4
+          Phase_correction =(angle(ssd_Ts_1)-angle(rl_Ts(1)));
+          rl_Ts = rl_Ts*exp(1i*Phase_correction);
+          rl_Ts = rl_Ts(3:end)./(rl_Ts(2:end-1));
+          rl_Ts=rl_Ts./max(abs(rl_Ts));
+          %disp(Phase_correction);      
+          rl_Ts_conc=[rl_Ts_conc, rl_Ts];
+          %figure, plot(rl_Ts,'*');
+
+end  
+
+%% Démodulateur numérique
+rl_Ts_conc=rl_Ts_conc(2:end);
+figure, plot(rl_Ts,'*');
+title('Constellation I/Q de r_{l}[n]')
+xlabel('I')
+ylabel ('Q')
+sb_esti = zeros(1,(Nb+Nb_paquets*length(Octet_verif)));
+sb_esti(2:2:end) = real(rl_Ts_conc)<0;
+sb_esti(1:2:end) = imag(rl_Ts_conc)<0;
+%% Verification de l'octet AA
+octet = zeros(8,Nb_paquets);
+
+for i=1:Nb_paquets    
+    octet(:,i) = sb_esti(1+(i-1)*length(Octet_verif)+(i-1)*Nb/Nb_paquets:i*length(Octet_verif)+(i-1)*Nb/Nb_paquets);
+end
+%disp(sb_esti(1:8));
+%% Supression de l'octet AA
+for j = Nb_paquets:-1:1     
+    sb_esti(1+(j-1)*length(Octet_verif)+(j-1)*Nb/Nb_paquets :j*length(Octet_verif)+(j-1)*Nb/Nb_paquets )=[];      
+end
+
+%% Enrengistrement
+
+for i=0:length(sb_esti)/8 -1
+   pixel_8(i+1) =  bin2dec(dec2bin(sb_esti(1+8*i:8+8*i))'); 
+
+end
+
+I_rgb = reshape(pixel_8,3,[]);
+I_rgb = I_rgb';
+I_r = reshape(I_rgb(:,1),rows,columns);
+I_g = reshape(I_rgb(:,2),rows,columns);
+I_b = reshape(I_rgb(:,3),rows,columns);
+
+
+I = cat(3,I_r,I_g,I_b);
+figure, imshow(uint8(I))
+
+%% Filtrage m�ridian 
+for c = 1 : 3
+    I(:, :, c) = medfilt2(I(:, :, c), [3, 3]);
+end
+figure; imshow(uint8(I));
+
+
+%% BER
+sb=load("sb.mat","sb_Te");
+sb=sb.sb_Te;
+[n,r]=biterr(sb,sb_esti);
+
+% Constellation I/Q
+subplot 142
+scatter(real(test),...
+        imag(test),'.');
+grid on
+title('Constellation I/Q de rl_{Te}[n]')
+xlabel('I')
+ylabel ('Q')
+subplot 144
+scatter(real(rl_Ts_conc),...
+        imag(rl_Ts_conc),'o');
+grid on
+title('Constellation I/Q de r_{l}[n]')
+xlabel('I')
+ylabel ('Q')
